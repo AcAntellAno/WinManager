c23a23a4a1aee2a43026d7a91d5534d86e3f4cf6
AcAntellAno <acantellano1996@gmail.com>
2018-08-17 13:56:58 -0700

    Added main file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <glog/loggin.h>
+#include "window_manager.hpp"
+
+using ::std::unique_ptr;
+
+int main(int argc, char** argv){
+	::google::InitGoogleLogging(argv[0]);
+
+	//add win code from header
+}
